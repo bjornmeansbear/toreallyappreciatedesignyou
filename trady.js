6e2813467667e731ad379e35f0f105e03d3e8a77
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-21 01:53:11 -0400

    cleaning some things up… updating JSON

--- trady.js
+++ trady.js
@@ -1,16 +1,12 @@
 
-$.getJSON('oblique.json', function(data) { 
+$.getJSON('trady.json', function(data) { 
     
-  var randomStrategy = data.strategies[Math.floor(Math.random()*data.strategies.length)];
-  console.log(randomStrategy.strategy);
-  $("#oblique_strategies p").text(randomStrategy.strategy);
+  var randomTrady = data.tradys[Math.floor(Math.random()*data.tradys.length)];
+  console.log(randomTrady.item);
+  $("#tradys p").text(randomTrady.item);
 
-  $.each(data.strategies, function(data) {
-    $('#fulllist').append('<li class="strategy"><p>' + this["strategy"] + '</p></li>');
-  });
-    
 });
 
-$('#oblique_strategies').click(function() {
+$('#tradys').click(function() {
   document.location.reload(true);
 });
