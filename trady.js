022490b585c062a7a883384dd4b008170b98ff62
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-21 01:59:14 -0400

    better?

--- trady.js
+++ trady.js
@@ -1,9 +1,9 @@
 
 $.getJSON('trady.json', function(data) { 
     
-  var randomTrady = data.tradys[Math.floor(Math.random()*data.tradys.length)];
-  console.log(randomTrady.item);
-  $("#tradys p").text(randomTrady.item);
+  var randomTrady = data.items[Math.floor(Math.random()*data.items.length)];
+  console.log(randomTrady);
+  $("#tradys p").text(randomTrady);
 
 });
 
